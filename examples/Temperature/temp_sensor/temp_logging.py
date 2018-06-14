0e9c27ed3a57085ad4a470107b9f3d9b4bb051eb
magnusoy <33287065+magnusoy@users.noreply.github.com>
2018-06-14 22:39:14 +0200

    Update temp_logging.py

--- examples/Temperature/temp_sensor/temp_logging.py
+++ examples/Temperature/temp_sensor/temp_logging.py
@@ -1,16 +1,15 @@
-from arduino_serial import ArduinoSerial
+from arduino_serial import Arduino
 
 filename = "temperature.csv"
 
 arduino = ArduinoSerial()
 
-arduino.createFile(filename)
+arduino.createLogfile(filename)
 
 arduino.connect("COM4", 9600)
 
 while arduino.isConnected():
-    t = arduino.getTimeElapsed()
-    y = arduino.cleanData()
-    arduino.logToFile(filename, t, y)
+    data = arduino.getData()
+    arduino.logDataWithTime(filename, data)
     if arduino.getTimeElapsed > 10:
         arduino.disconnect()
