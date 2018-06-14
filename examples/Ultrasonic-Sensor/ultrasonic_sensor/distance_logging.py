28bbc9666bc540def9b893e1f6a9054962d5bca4
magnusoy <33287065+magnusoy@users.noreply.github.com>
2018-06-14 22:42:24 +0200

    Update distance_logging.py

--- examples/Ultrasonic-Sensor/ultrasonic_sensor/distance_logging.py
+++ examples/Ultrasonic-Sensor/ultrasonic_sensor/distance_logging.py
@@ -1,16 +1,15 @@
-from arduino_serial import ArduinoSerial
+from arduino import Arduino
 
 filename = "temperature.csv"
 
-arduino = ArduinoSerial()
+arduino = Arduino()
 
 arduino.createFile(filename)
 
 arduino.connect("COM4", 9600)
 
 while arduino.isConnected():
-    t = arduino.getTimeElapsed()
-    y = arduino.cleanData()
-    arduino.logToFile(filename, t, y)
+    data = arduino.getData()
+    arduino.logDataWithTime(filename, data)
     if arduino.getTimeElapsed > 10:
         arduino.disconnect()
